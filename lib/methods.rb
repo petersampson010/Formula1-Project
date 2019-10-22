b07a7893a5e4289d8ce690f1bef5982af84a1929
flow1981 <florian.stagliano@gmx.de>
2019-10-22 19:20:58 +0100

    minor restructuring of methods.rb > order of methods

--- lib/methods.rb
+++ lib/methods.rb
@@ -17,6 +17,77 @@ def pre_season
     select_drivers
 end
 
+
+
+# PREPARE SEASON
+#---------------
+
+def introduction 
+  puts "Formula1-Fantasy"
+  puts "You've just won the lottery! What will you do with it..."
+  puts "Build an F1 Team of course!"
+  puts "Create your team, pick your drivers and you're ready for the new season ahead"
+end 
+
+def create_team 
+  puts "Please enter your team name:"
+  team_name = gets.strip 
+  puts "Please enter the country your team has originated from:"
+  team_nationality = gets.strip
+  team = Constructor.create(name: team_name, nationality: team_nationality, tech_factor: 0.5)
+  team.id
+  #users_team = Constructor.find_by(id: 9)
+end 
+
+def list_drivers 
+  puts `clear`
+  puts "Now its time to hire some drivers for your season!" 
+  puts "Every drivers desperate to be on your books but watch out, that lottery fund of £100m will only take you so far and once a drivers signed that contract hes in for the season!"
+  sleep(2)
+  Driver.all.each {|d| puts "#{d.id}. #{d.first_name} #{d.second_name} | Age:#{d.age} | #{d.nationality} | Price:#{d.price}"}
+end 
+
+def my_team
+  Constructor.find_by(id: 9)
+end 
+
+def pick_driver1
+  Constructor.find_by(id: team_id).pick_driver
+end 
+
+def pick_driver2
+  puts "choose your second driver"
+  driver2_id = gets.strip 
+  driver2 = Driver.find_by(id: driver2_id)
+  enough_money?
+end 
+
+def select_drivers 
+  pick_driver1
+  #potentially list_drivers again 
+  pick_driver2
+end 
+
+def enough_money?
+  budget = (14 - driver1.price)
+  if driver2.price > budget 
+      "You havent got enough for this driver, please select again"
+      pick_driver2 
+  else 
+      puts "And there it is, the lineup for #{users_team} is:"
+      puts "#{driver1.id}. #{driver1.first_name} #{driver1.second_name} | #{driver1.age} | #{driver1.nationality} | #{driver1.price}"
+      puts "#{driver2.id}. #{driver2.first_name} #{driver2.second_name} | #{driver2.age} | #{driver2.nationality} | #{driver2.price}"
+  end 
+end 
+
+
+def new_game 
+  introduction 
+  team_id = create_team
+  list_drivers
+  select_drivers
+end 
+
 def points_per_position(position)
   case position
   when 1
@@ -40,6 +111,7 @@ def points_per_position(position)
   end
 end
 
+
 # RUN THE SEASON
 #----------------
 
@@ -55,70 +127,7 @@ def show_current_driver_standings
   Driver.all.show_standings
 end
 
-def new_game 
-    introduction 
-    team_id = create_team
-    list_drivers
-    select_drivers
-end 
 
-def introduction 
-    puts "Formula1-Fantasy"
-    puts "You've just won the lottery! What will you do with it..."
-    puts "Build an F1 Team of course!"
-    puts "Create your team, pick your drivers and you're ready for the new season ahead"
-end 
-
-def create_team 
-    puts "Please enter your team name:"
-    team_name = gets.strip 
-    puts "Please enter the country your team has originated from:"
-    team_nationality = gets.strip
-    team = Constructor.create(name: team_name, nationality: team_nationality, tech_factor: 0.5)
-    team.id
-    #users_team = Constructor.find_by(id: 9)
-end 
-
-def list_drivers 
-    puts `clear`
-    puts "Now its time to hire some drivers for your season!" 
-    puts "Every drivers desperate to be on your books but watch out, that lottery fund of £100m will only take you so far and once a drivers signed that contract hes in for the season!"
-    sleep(2)
-    Driver.all.each {|d| puts "#{d.id}. #{d.first_name} #{d.second_name} | Age:#{d.age} | #{d.nationality} | Price:#{d.price}"}
-end 
-
-def my_team
-    Constructor.find_by(id: 9)
-end 
-
-def pick_driver1
-    Constructor.find_by(id: team_id).pick_driver
-end 
-
-def pick_driver2
-    puts "choose your second driver"
-    driver2_id = gets.strip 
-    driver2 = Driver.find_by(id: driver2_id)
-    enough_money?
-end 
-
-def select_drivers 
-    pick_driver1
-    #potentially list_drivers again 
-    pick_driver2
-end 
-
-def enough_money?
-    budget = (14 - driver1.price)
-    if driver2.price > budget 
-        "You havent got enough for this driver, please select again"
-        pick_driver2 
-    else 
-        puts "And there it is, the lineup for #{users_team} is:"
-        puts "#{driver1.id}. #{driver1.first_name} #{driver1.second_name} | #{driver1.age} | #{driver1.nationality} | #{driver1.price}"
-        puts "#{driver2.id}. #{driver2.first_name} #{driver2.second_name} | #{driver2.age} | #{driver2.nationality} | #{driver2.price}"
-    end 
-end 
 
 
 def prepare_next_race
@@ -138,11 +147,3 @@ def run_season
 
 end
 
-
-## RUN GAME
-##---------
-
-# def  play_game 
-#     pre_season
-#     season
-# end
