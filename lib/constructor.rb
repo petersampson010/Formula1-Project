0c9d80ac5b10040309ba945542025b921e4e591e
Peter Sampson <peter.sampson010@gmail.com>
2019-10-22 17:40:23 +0100

    added pick_driver method

--- lib/constructor.rb
+++ lib/constructor.rb
@@ -2,16 +2,9 @@ class Constructor < ActiveRecord::Base
 
     has_many :drivers
 
-    def pick_driver 
+    def pick_driver(driver1)
+        self.drivers << driver1
     end 
 
-    # def pick_driver1
-    #     puts "Take a look through all the drivers and select one by entering their number:"
-    #     driver1_id = gets.strip 
-    #     driver1 = Driver.find_by(id: driver1_id)
-    #     driver1.constructor_id = team_id
-    #     Constructor.find_by(id: team_id).drivers << driver1
-    #     puts "Great choice, #{driver1.first_name} has been on fire pre-season! You now have #{14 - driver1.price} to spend on your second driver"
-    # end 
 
 end 
