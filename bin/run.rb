978d094e636801a652cdc0c4283c39d15d7b6148
Peter Sampson <peter.sampson010@gmail.com>
2019-10-22 11:15:26 +0100

    if driver input is invalid

--- bin/run.rb
+++ bin/run.rb
@@ -4,3 +4,4 @@ require_relative '../config/environment'
 
 puts "HELLO WORLD"
 
+
--- lib/methods.rb
+++ lib/methods.rb
@@ -8,7 +8,7 @@ def new_or_load
     else 
         new_or_load
     end 
-end 
+end
 
 def new_game 
     puts "Introduction and explanation of game and how they can select/improve their team, also how they can get help if needed"
@@ -17,15 +17,28 @@ def new_game
     **POTENTIAL TO CHANGE NAME IF TEHY MISSPELL**
     puts "please enter your team nationality"
     team_nationality = gets.strip 
-    team_name = Constructor.new(name: team_name, nationality: team_nationality, tech_factor: 0.1)
+    team_name = Constructor.new????create(name: team_name, nationality: team_nationality, tech_factor: 0.1)
     puts "now its time to pick your drivers!"
-    list_drivers 
-
-
-
+    list_drivers
+    select_drivers 
+    puts "take a look through all the drivers and select two by entering their number: Be careful though, your a new team so only have a starting budget of £100"
+    puts 
 end 
 
 
 
 def list_drivers 
-    Driver.all.map {|d| puts "#{driver.id}. #{driver.first_name} #{second_name} | {"}
+    Driver.all.map {|d| puts "#{driver.id}. #{driver.first_name} #{driver.second_name} | #{driver.age} | #{driver.nationality} | #{driver.price}"}
+end 
+
+def select_drivers 
+    puts "take a look through all the drivers and select two by entering their number: Be careful though, your a new team so only have a starting budget of £100"
+    puts "driver1:"
+    driver1_id = gets.strip 
+#what happens if input is invalid 
+    driver1 = Driver.find_by(id: driver1_id)
+    puts "you have selected #{driver1.name}"
+    puts "driver2:"
+    driver2_id = gets.strip
+    driver2 = Driver.find_by(id: driver2_id)
+
