a693e0e4180d558be134e16a62037b1b6103e876
flow1981 <florian.stagliano@gmx.de>
2019-10-23 21:47:31 +0100

    chnages to game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -24,57 +24,66 @@ class Game < ActiveRecord::Base
     end 
 
     def game_drivers 
-        Driver.all.select {|d| d.game_id == self.id}
+      Driver.all.select {|d| d.game_id == self.id}
+      # returns an array of driver instances
     end 
 
     def game_constructors 
-        Constructor.all.select {|c| c.game_id == self.id}
+      Constructor.all.select {|c| c.game_id == self.id}
+      # returns an array of constructor instances
     end 
 
     def our_team 
-        game_constructors.select {|c| c.id == @our_team_id}
+      game_constructors.select {|c| c.id == @our_team_id}
+      # returns an instance
     end 
 
     def available_drivers 
-        game_drivers.reject {|d| d.first_name == "Lance" || d.first_name == "Sergio"}
+      game_drivers.reject { |d| d.first_name == "Lance" || d.first_name == "Sergio" }
+      # returns an array of instances
+    end
+
+    def available_drivers_by_budget(budget)
+      game_drivers.reject { |d| d.first_name == "Lance" || d.first_name == "Sergio" || d.price > budget }
+      # returns an array of instance
     end
 
     def map_drivers
-        available_drivers.each_with_index.map {|d, index| [index+1, d]}
+      available_drivers.each_with_index.map { |d, index| [index + 1, d] }
+      # returns an array of arrays
     end 
 
-    # puts "#{index+1}. #{d.first_name} #{d.second_name} | Age:#{d.age} | #{d.nationality} | Price:#{d.price}"
-    # end 
-
-    def show_drivers 
-        map_drivers.each do |d| 
-            puts "#{d[0]}. #{d[1].first_name} #{d[1].second_name} | Age:#{d[1].age} | #{d[1].nationality} | Price:#{d[1].price}"
-        end 
+    def show_drivers(drivers) 
+      map_drivers.each do |d| 
+        puts "#{d[0]}. #{d[1].first_name} #{d[1].second_name} | Age:#{d[1].age} | #{d[1].nationality} | Price:#{d[1].price}"
+      end 
+      # return nil
     end 
-
-
 
     def list_drivers 
-        puts `clear`
-        puts "Now its time to hire some drivers for your season!" 
-        puts "Every drivers desperate to be on your books but watch out, that lottery fund of £100m will only take you so far and once a drivers signed that contract hes in for the season!"
-        sleep(2)
-        show_drivers
-    end 
-
-    def select_drivers 
-        puts "please select the number of the driver you'd like to join your team:"
-        
-       
-
-
+      puts `clear`
+      puts "Now its time to hire some drivers for your season!" 
+      puts "Every drivers desperate to be on your books but watch out,"
+      puts "that lottery fund of £100m will only take you so far and once a drivers signed that contract hes in for the season!"
+      sleep(2)
+      show_drivers
+      driver_one_selection
+      driver_two_selection
+      present_lineup   
     end 
 
     def driver_one_selection
-        driver1_id = gets.strip
-        driver1 = game_drivers[driver1_id - 1]
+        puts "please select the number of the driver you'd like to join your team:"
+        list_num = gets.strip
+        driver1 = game_drivers[list_num - 1]
         self.our_team.pick_driver(driver1)
         puts "Great choice! #{self.our_team.drivers[0].first_name} has been on fire pre-season. You have #{self.our_team.budget} left for your next driver:"
+    end
+
+    def driver_two_selection
+
+    end
+
         driver2_id = gets.strip 
         until driver2_id != driver1_id do
             if enough_money? 
@@ -82,10 +91,7 @@ class Game < ActiveRecord::Base
                 self.our_team.pick_driver(driver2)
             else 
                 puts "You havent got the money for this driver, please select again:"
-            
-
-
-
+        
     end 
 
     def driver_two_selection 
@@ -95,13 +101,6 @@ class Game < ActiveRecord::Base
         
     end
 
-def select_drivers(users_team) 
-    driver_one_selection(users_team)
-    driver_two_selection(users_team)
-    puts "And there it is, the lineup for #{users_team.name} is:"
-    puts "#{users_team.drivers[0].first_name} #{users_team.drivers[0].second_name}"
-    puts "#{users_team.drivers[1].first_name} #{users_team.drivers[1].second_name}"
-end 
 
 
 def driver_two_selection(users_team)
@@ -129,5 +128,9 @@ def enough_money?(users_team, driver)
     end 
 end 
 
-
+def present_lineup
+    puts "And there it is, the lineup for #{users_team.name} is:"
+    puts "#{users_team.drivers[0].first_name} #{users_team.drivers[0].second_name}"
+    puts "#{users_team.drivers[1].first_name} #{users_team.drivers[1].second_name}"
+end
 
