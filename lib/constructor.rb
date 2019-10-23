a706de7212251b974201890a853ccaccdcabb454
flow1981 <florian.stagliano@gmx.de>
2019-10-23 10:44:45 +0100

    merge conflicts resolved

--- lib/constructor.rb
+++ lib/constructor.rb
@@ -1,11 +1,9 @@
 class Constructor < ActiveRecord::Base
 
-<<<<<<< HEAD
+
     has_many :drivers
     has_one :game
-=======
-  has_many :drivers
->>>>>>> 0919_simulaterace
+
 
   def pick_driver(driver)
     drivers << driver
